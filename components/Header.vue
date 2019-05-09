b5c68a95ec611169dbf943abf1e74c8e3b63a25a
Anthony Hook <antone4hook@gmail.com>
2019-05-09 16:15:00 +0900

    feat: Added Header with navigation
    Created a generic About page to route between using a Bootstrap-Vue b-nav that toggles active property based on current route.

---
+++ components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <b-nav tabs fill>
+      <b-nav-item v-bind:active="currentPath === '/' ? true : false">
+        <nuxt-link v-on:click.native="navItemHandler" to="/">Comic Books</nuxt-link>
+      </b-nav-item>
+      <b-nav-item v-bind:active="currentPath === '/about' ? true : false">
+        <nuxt-link v-on:click.native="navItemHandler" to="/about">About</nuxt-link>
+      </b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      currentPath: $nuxt.$route.path
+    };
+  },
+  methods: {
+    navItemHandler: function() {
+      this.currentPath = $nuxt.$route.path;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.nav-link a {
+  color: #6c757d;
+}
+.active a {
+  color: #007bff;
+}
+</style>
+
+
+
+
